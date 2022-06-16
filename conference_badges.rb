c2bfcbbc53fefa76e02c20b0b74ef989098227b6
Irene <irenemanningm@gmail.com>
2022-06-15 20:00:12 -0500

    commit tree 2a30e9474fc36f93e397739d5526b2c9646c1094

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+require "pry"
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map { |name| "Hello, my name is #{name}." }
+end
+
+def assign_rooms(array)
+    array.map.with_index { |name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer (array)
+    array.map { |name| puts "Hello, my name is #{name}." }
+    assign_rooms(array).map { |assingment| puts assingment }
+end
+
